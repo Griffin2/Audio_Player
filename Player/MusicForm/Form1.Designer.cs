a8370c09ff68409ec3090e129620fca6e0b7a8f8
Griffin Toal <toalgriffin@gmail.com>
2018-04-17 09:04:18 -0500

    Add files via upload

---
+++ Player/MusicForm/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace MusicForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.openButton.ForeColor = System.Drawing.Color.Blue;
+            this.openButton.Location = new System.Drawing.Point(439, 276);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(607, 129);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open ";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.playButton.ForeColor = System.Drawing.Color.Green;
+            this.playButton.Location = new System.Drawing.Point(25, 276);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(323, 129);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "PLAY";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.txtFileName.Location = new System.Drawing.Point(25, 148);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            this.txtFileName.Size = new System.Drawing.Size(1076, 91);
+            this.txtFileName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(22, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(425, 95);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1116, 538);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.openButton);
+            this.ForeColor = System.Drawing.Color.Maroon;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "WAV MEDIA PLAYER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Player/MusicForm/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicForm
+{
+    public partial class Form1 : Form
+    {
+        //declare and  create object
+        private SoundPlayer player;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            //open file location box
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "WAV|*.wav", Multiselect = false, ValidateNames = true })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    //display file location
+                    txtFileName.Text = ofd.FileName;
+                }
+            }
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFileName.Text))
+            {
+                return;
+            }
+            try
+            {
+                if (playButton.Text == "PLAY")
+                {
+                    //play button
+                    player.SoundLocation = txtFileName.Text;
+                    player.PlayLooping();
+                    playButton.Text = "STOP";
+                }
+                else if (playButton.Text == "STOP")
+                {
+                    //stop button
+                    player.Stop();
+                    playButton.Text = "PLAY";
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            player = new SoundPlayer();
+        }
+    }
+}
